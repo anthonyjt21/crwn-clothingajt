c5c3e58a1554bf9f95cf98a2f48218da035591db
Anthony Jimenez <ajimenez@kilambe.dga.gob.ni>
2020-01-14 12:25:39 -0600

    withRouter

--- src/components/menu-item/menu-item-components.jsx
+++ src/components/menu-item/menu-item-components.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, history}) => (
   <div className={`${size} menu-item`}>
     <div
        className='background-image'
@@ -18,4 +19,4 @@ const MenuItem = ({title, imageUrl, size}) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
