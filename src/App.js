09d13044a949d09fe6991da11f7934124ec037b8
Anthony Jimenez <ajimenez@kilambe.dga.gob.ni>
2020-01-14 12:14:12 -0600

    ejemplo de navegacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+// import HomePage from './pages/homepage/homepage.component'
 import {
-  Switch, Route
+  Switch, Route, Link
 } from 'react-router-dom';
 
-const HatsPage = () => (
+const HomePage = (props) =>{
+  console.log(props) 
+  return (
   <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+      {/* <Link to='/topics'>Topics</Link> */}
+      <button onClick ={() => props.history.push('/blog/asdq/topics')}>Topics</button>
+    <h1>Home Page</h1>
+  </div>)
+}
+const TopicsList = (props) => {
+  // console.log(props)
+    return (   
+    <div>    
+      <h1>Topic List Pagde</h1>
+      <Link to={`${props.match.url}/13`}>To Topic 13</Link>
+      <Link to={`${props.match.url}/17`}>To Topic 17</Link>
+      <Link to={`${props.match.url}/21`}>To Topic 21</Link>    
+      <Link to={`${props.match.url}/22`}>To Topic 21</Link>    
+    </div>
+  )
+ 
+}
+
+const TopicDetail = (props) => {
+  // console.log(props)
+   return (
+     <div>
+<h1>Topic Detail Page: {props.match.params.topicId}</h1>
+     </div>
+   )
+}
 
 
 function App() {
   return (
-    <div>
-      <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />   
-      </Switch> 
+    <div>      
+      <Route  exact path='/' component={HomePage} />
+      <Route exact path='/blog/asdq/topics' component={TopicsList} />   
+      <Route  path='/blog/asdq/topics/:topicId' component={TopicDetail} />          
+      <Route exact path='/blog/topics' component={TopicsList} />   
+      <Route  path='/blog/topics/:topicId' component={TopicDetail} />          
     </div>
   );
 }
