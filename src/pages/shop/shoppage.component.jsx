e974fb35c7f134e4d462623492aa7cd19d3b7b86
ajimenez <ajimenez>
2020-02-13 10:06:44 -0600

    build own collecion overview components

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview'
+import CollectionsOverView from '../../components/collections-overview/collections-overview.components';
 
-import {selectCollections} from '../../redux/shop/shop.selectors'
 
 const ShopPage = ({collections }) => (     
       <div className='shop-page'>
-        {collections.map(({id, ...otherCollectionsProps})  => (
-            <CollectionPreview key={id} {...otherCollectionsProps} />
-          ) )
-        }
+       <CollectionsOverView/>
       </div>
   ); 
 
-  const mapStateToProps = createStructuredSelector(
-    {
-      collections: selectCollections
-    }
-  )
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
